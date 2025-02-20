3e819337b0534c9d158971538bdf4acd89aa9041
Tushar Singh Bisht <47006416+iam-tusharsinghbisht@users.noreply.github.com>
2025-02-20 16:16:43 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,10 +1,11 @@
-def call(String ProjecName, String ImageTag, String DockerCred){
+def call(String ProjecName, String ImageTag, String DockerUsername String DockerCred){
   withCredentials([usernamePassword(credentialsId: DockerCred, 
                                                  usernameVariable: 'USERNAME', 
                                                  passwordVariable: 'PASSWORD')]){
     // Securely log in to Docker
-    sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'// Push the Docker image
-    sh 'echo "Pusing Docker Image"'
-    sh "docker push $USERNAME/${ProjectName}:${ImageTag}"
+    sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
   }
+  // Push the Docker image
+  sh 'echo "Pusing Docker Image"'
+  sh "docker push ${DockerUsername}/${ProjectName}:${ImageTag}"
 }
